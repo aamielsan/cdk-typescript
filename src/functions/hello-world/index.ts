3de277801723b154c1cbad7c51fe8d14afb9f053
Adrian Sanchez <aamiel.sanchez@gmail.com>
2022-07-13 20:51:32 +0200

    Add lambda and check dynamodb global table

--- src/functions/hello-world/index.ts
+++ src/functions/hello-world/index.ts
@@ -1,3 +1,24 @@
-export function handler() {
-    console.log("Hello world!")
+import { DynamoDBClient, ScanCommand } from "@aws-sdk/client-dynamodb"
+
+const client = new DynamoDBClient({});
+
+const tableArn = process.env["TABLE_ARN"];
+const tableName = process.env["TABLE_NAME"];
+const tableStreamArn = process.env["TABLE_STREAM_ARN"];
+
+export async function handler() {
+    console.log({
+        tableArn,
+        tableName,
+        tableStreamArn,
+    });
+
+    try {
+        const response = await client.send(new ScanCommand({
+            TableName: tableName,
+        }));
+        console.log(`Database items: ${response.Items}`);
+    } catch (err) {
+        console.log(`Error:`, err);
+    }
 }
